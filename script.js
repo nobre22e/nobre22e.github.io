bd64e207b65b63bc87e6142003fb73cb9e37a881
Emilia Nobrega <nobre22e@mtholyoke.edu>
2020-03-18 10:32:44 -0400

    Updated Layout and script
    I changed the layout of the website and changed the script to one that does not mysteriously speed up courtesy of @gschier on CodePen. More updates with content to come!

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-// OnLoad
-window.onload = function () {
-	var animation = Animate();
-	animation.beginAnimation();
-};
-
-// classes
-
-function Animate() {
-	var experiences = ['Mount Holyoke student', 'Microsoft Garage Alumni', 'radio show host', 'Dungeons and Dragons player'];
-	var links = ['','','',''];
-	var index = 0;
-	var next = function() {
-		index = index + 1 < experiences.length ? index + 1 : 0;
-	};
-
-	var experienceElt = $('#experience')
-
-	var animateExperiences = function() {
-		experienceElt.fadeOut(200, function () {
-			next();
-			experienceElt.text(experiences[index]);
-		}).fadeIn(200);
-	};
-
-	this.beginAnimation = function() {
-		var timer = setTimeout(function tick(){
-			animateExperiences();
-			timer = setTimeout(tick, 2000)
-		}, 2000);
-	};
-	return this;
-}
+// CodePen by @gschier
+var TxtRotate = function(el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 2000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
+  
+	if (this.isDeleting) {
+	  this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+	  this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
+  
+	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+	var that = this;
+	var delta = 300 - Math.random() * 100;
+  
+	if (this.isDeleting) { delta /= 2; }
+  
+	if (!this.isDeleting && this.txt === fullTxt) {
+	  delta = this.period;
+	  this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+	  this.isDeleting = false;
+	  this.loopNum++;
+	  delta = 500;
+	}
+  
+	setTimeout(function() {
+	  that.tick();
+	}, delta);
+  };
+  
+  window.onload = function() {
+	var elements = document.getElementsByClassName('txt-rotate');
+	for (var i=0; i<elements.length; i++) {
+	  var toRotate = elements[i].getAttribute('data-rotate');
+	  var period = elements[i].getAttribute('data-period');
+	  if (toRotate) {
+		new TxtRotate(elements[i], JSON.parse(toRotate), period);
+	  }
+	}
+	// INJECT CSS
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+	document.body.appendChild(css);
+  };
+  
